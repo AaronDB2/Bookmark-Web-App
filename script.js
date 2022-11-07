b95aba27f8cc1411df5d2a21dff309d1a67a1acb
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-07 15:55:33 +0100

    DOM now populates all the bookmarks when adding new ones and added functionality for deleting bookmarks

--- script.js
+++ script.js
@@ -35,6 +35,41 @@ function validate(nameValue, urlValue) {
 	return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+	// Remove all bookmark elements
+	bookmarksContainer.textContent = '';
+	// Build items
+	bookmarks.forEach((bookmark) => {
+		const { name, url } = bookmark;
+		// Item
+		const item = document.createElement('div');
+		item.classList.add('item');
+		// Close Icon
+		const closeIcon = document.createElement('i');
+		closeIcon.classList.add('fas', 'fa-times');
+		closeIcon.setAttribute('title', 'Delete Bookmark');
+		closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`)
+		// Favicon / Link Container
+		const linkInfo = document.createElement('div');
+		linkInfo.classList.add('name');
+		// Favicon
+		const favicon = document.createElement('img');
+		favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+		favicon.setAttribute('alt', 'Favicon');
+		// Link
+		const link = document.createElement('a');
+		link.setAttribute('href', `${url}`);
+		link.setAttribute('target', '_blank');
+		link.textContent = name;
+
+		// Append to bookmarks container
+		linkInfo.append(favicon, link);
+		item.append(closeIcon, linkInfo);
+		bookmarksContainer.appendChild(item);
+	});
+}
+
 // Fetch Bookmarks
 function fetchBookmarks() {
 	// Get bookmarks from localStorage if available
@@ -50,6 +85,19 @@ function fetchBookmarks() {
 		];
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
+	buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+	bookmarks.forEach((bookmark, index) => {
+		if (bookmark.url === url){
+			bookmarks.splice(index, 1);
+		}
+	});
+	// Update bookmarks array in localStorage, re-pop DOM
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
 }
 
 // Handle Data from Form
