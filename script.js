25d76a7d6e7a52390de2293354e24e2649c9c496
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-07 15:25:57 +0100

    Added bookmarks to localStorage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal() {
 	modal.classList.add('show-modal');
@@ -33,6 +35,23 @@ function validate(nameValue, urlValue) {
 	return true;
 }
 
+// Fetch Bookmarks
+function fetchBookmarks() {
+	// Get bookmarks from localStorage if available
+	if (localStorage.getItem('bookmarks')) {
+		bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	} else {
+		// Create bookmarks array in localStorage
+		bookmarks = [
+			{
+				name: 'Youtube',
+				url: 'https://www.youtube.com/',
+			}
+		];
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	}
+}
+
 // Handle Data from Form
 function storeBookmark (e) {
 	e.preventDefault();
@@ -41,11 +60,24 @@ function storeBookmark (e) {
 	if (!urlValue.includes('http://') && !urlValue.includes('https://')) {
 		urlValue = `https://${urlValue}`;
 	}
-	console.log(urlValue);
+
 	if (!validate(nameValue, urlValue)) {
 		return false;
 	}
+
+	const bookmark = {
+		name: nameValue,
+		url: urlValue,
+	};
+	bookmarks.push(bookmark);
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
+	bookmarkForm.reset();
+	websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load, Fetch Bookmarks
+fetchBookmarks();
